ee7626942e78fb32b77033072e558ffdf8c2a52e
lanyeeee <1210347077@qq.com>
2024-06-29 09:22:14 +0800

    feat: 生成白底和黑底水印图

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,72 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod types;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn generate_black_or_white(
+    image_path: &str,
+    rect_data: types::RectData,
+    is_black: bool,
+) -> Result<(), ()> {
+    let mut img = image::open(image_path).unwrap().to_rgb8();
+    // 获取图片的宽高
+    let (width, height) = img.dimensions();
+    // 获取矩形框的左上角和右下角坐标
+    let (left, top) = (rect_data.left, rect_data.top);
+    let (right, bottom) = (rect_data.right, rect_data.bottom);
+    // 用于统计矩形框内每种颜色的像素点数量
+    let mut color_count = std::collections::HashMap::new();
+    // 统计矩形框内每种颜色的像素点数量
+    for y in top..=bottom {
+        for x in left..=right {
+            let pixel = img.get_pixel(x, y);
+            let count = color_count.entry(pixel).or_insert(0);
+            *count += 1;
+        }
+    }
+    // 找出出现次数最多的RGB值，即矩形框内的主要颜色
+    let dominant_rgb = *color_count
+        .into_iter()
+        .max_by_key(|(_, count)| *count)
+        .unwrap()
+        .0;
+    // 将不在矩形框内的其他像素点设为主要颜色
+    for y in 0..height {
+        for x in 0..width {
+            if x < left || x > right || y < top || y > bottom {
+                img.put_pixel(x, y, dominant_rgb);
+            }
+        }
+    }
+    // 保存图片
+    if is_black {
+        img.save("black.png").unwrap();
+    } else {
+        img.save("white.png").unwrap();
+    }
+    Ok(())
+}
+
+#[tauri::command]
+fn read_file(path: String) -> Result<Vec<u8>, String> {
+    std::fs::read(&path).map_err(|err| err.to_string())
+}
+
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            generate_black_or_white,
+            read_file,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/components/WatermarkPicker.vue
@@ -0,0 +1,144 @@
+<script setup lang="ts">
+import {computed, onMounted, Ref, ref} from 'vue';
+import {RectData} from "../types.ts";
+import {invoke} from "@tauri-apps/api/core";
+import {open} from "@tauri-apps/plugin-dialog";
+
+const props = defineProps<{
+  isBlack: boolean;
+}>();
+
+const MASKER_OPACITY = 0.7;
+const maskerValue = computed(() => props.isBlack ? 255 : 0);
+
+const canvasContainer: Ref<HTMLDivElement | null> = ref(null);
+const canvas: Ref<HTMLCanvasElement | null> = ref(null);
+const srcImage: HTMLImageElement = new Image();
+let rectData: RectData = {left: 0, top: 0, right: 0, bottom: 0,};
+let srcImagePath: string = "";
+
+
+onMounted(() => {
+  if (canvas.value === null) {
+    console.error('canvas is null')
+    return;
+  }
+
+  const ctx = canvas.value.getContext('2d');
+  if (ctx === null) {
+    console.error('ctx is null')
+    return;
+  }
+  // 图片加载完成后
+  srcImage.onload = () => {
+    if (canvasContainer.value === null || canvas.value === null) {
+      console.error('canvasContainer or canvas is null')
+      return;
+    }
+    //设置canvas大小
+    canvas.value.width = srcImage.width;
+    canvas.value.height = srcImage.height;
+    canvasContainer.value.style.width = `${srcImage.width}px`;
+    canvasContainer.value.style.height = `${srcImage.height}px`;
+    canvasContainer.value.style.display = 'block';
+    // 绘制图片，并设置masker
+    ctx.fillStyle = `rgba(${maskerValue.value}, ${maskerValue.value}, ${maskerValue.value}, ${MASKER_OPACITY})`;
+    ctx.fillRect(0, 0, canvas.value.width, canvas.value.height);
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.drawImage(srcImage, 0, 0, srcImage.width, srcImage.height);
+  };
+});
+
+async function selectImage() {
+  const fileResponse = await open();
+  if (fileResponse === null) {
+    // 用户取消选择文件
+    return;
+  }
+
+  srcImagePath = fileResponse.path;
+
+  const imageData: ArrayBuffer = await invoke("read_file", {path: srcImagePath});
+  // 转换为 Base64
+  const base64 = btoa(
+      new Uint8Array(imageData).reduce(
+          (data, byte) => data + String.fromCharCode(byte),
+          ''
+      )
+  );
+
+  // 创建数据 URL 并更新 imageSrc
+  srcImage.src = `data:image/jpeg;base64,${base64}`;
+}
+
+function handleMouseDown(event: MouseEvent) {
+  if (canvas.value === null) {
+    console.error('canvas is null')
+    return;
+  }
+  // 记录截图起始位置
+  rectData.left = event.offsetX;
+  rectData.top = event.offsetY;
+
+  const ctx = canvas.value.getContext('2d');
+  if (ctx === null) {
+    console.error('ctx is null')
+    return;
+  }
+  // 定义鼠标移动和鼠标释放事件的处理函数
+  const moveEventHandler = (e: MouseEvent) => handleMouseMove(e, ctx);
+  const upEventHandler = () => handleMouseUp(moveEventHandler, upEventHandler);
+  // 为鼠标移动和释放事件添加事件监听器
+  canvas.value.addEventListener('mousemove', moveEventHandler);
+  canvas.value.addEventListener('mouseup', upEventHandler);
+}
+
+function handleMouseMove(event: MouseEvent, ctx: CanvasRenderingContext2D) {
+  if (canvas.value === null) {
+    console.error('canvas is null')
+    return;
+  }
+  // 根据鼠标移动更新右下角坐标
+  rectData.right = event.offsetX
+  rectData.bottom = event.offsetY
+  // 重置canvas
+  ctx.reset();
+  // 在整个canvas上绘制masker
+  ctx.fillStyle = `rgba(${maskerValue.value}, ${maskerValue.value}, ${maskerValue.value}, ${MASKER_OPACITY})`;
+  ctx.fillRect(0, 0, canvas.value.width, canvas.value.height);
+  // 切出截图区域，截图区域内的内容不受masker影响
+  ctx.globalCompositeOperation = 'destination-out';
+  ctx.fillStyle = '#bbb';
+  ctx.fillRect(
+      rectData.left,
+      rectData.top,
+      rectData.right - rectData.left,
+      rectData.bottom - rectData.top
+  );
+  // 重新绘制图片，使masker生效
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.drawImage(srcImage, 0, 0, srcImage.width, srcImage.height);
+}
+
+function handleMouseUp(moveEventHandler: (event: MouseEvent) => void, upEventHandler: () => void) {
+  // 移除鼠标移动和释放事件的监听器
+  canvas.value?.removeEventListener('mousemove', moveEventHandler);
+  canvas.value?.removeEventListener('mouseup', upEventHandler);
+}
+
+async function onConfirm() {
+  await invoke('generate_black_or_white', {imagePath: srcImagePath, rectData: rectData, isBlack: props.isBlack})
+}
+
+</script>
+
+<template>
+  <div>
+    <button @click="selectImage">选择图片</button>
+    <div ref="canvasContainer" class="canvasContainer" style="display: none;">
+      <canvas ref="canvas" @mousedown="handleMouseDown"/>
+    </div>
+    <button @click="onConfirm">确定</button>
+  </div>
+
+</template>
