9c922de7f31478a1dfa969daf6fe532cf16da67a
lanyeeee <1210347077@qq.com>
2024-06-30 04:09:17 +0800

    feat: 去水印功能

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod types;
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::{fs, thread::sleep};
 
-#[tauri::command]
-async fn generate_background(
-    image_path: &str,
-    rect_data: types::RectData,
-    is_black: bool,
-) -> Result<(), ()> {
+use image::{ImageBuffer, RgbImage};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+mod types;
+
+#[tauri::command(async)]
+fn generate_background(image_path: &str, rect_data: types::RectData, is_black: bool) {
     let mut img = image::open(image_path).unwrap().to_rgb8();
     // 获取图片的宽高
     let (width, height) = img.dimensions();
@@ -36,7 +32,7 @@ async fn generate_background(
         .max_by_key(|(_, count)| *count)
         .unwrap()
         .0;
-    // 将不在矩形框内的其他像素点设为主要颜色
+    // 将不在矩形框内的其他像素点设为背景颜色
     for y in 0..height {
         for x in 0..width {
             if x < left || x > right || y < top || y > bottom {
@@ -44,13 +40,12 @@ async fn generate_background(
             }
         }
     }
-    // 保存图片
+    // 保存生成的背景图片
     if is_black {
         img.save("black.png").unwrap();
     } else {
         img.save("white.png").unwrap();
     }
-    Ok(())
 }
 
 #[tauri::command]
@@ -58,14 +53,86 @@ fn read_file(path: String) -> Result<Vec<u8>, String> {
     std::fs::read(&path).map_err(|err| err.to_string())
 }
 
+#[tauri::command(async)]
+fn remove_watermark(white_path: &str, black_path: &str, manga_dir: &str, output_dir: &str) {
+    let white = image::open(white_path).unwrap().to_rgb8();
+    let black = image::open(black_path).unwrap().to_rgb8();
+    let manga_dir = std::path::Path::new(manga_dir);
+    let output_dir = std::path::Path::new(output_dir);
+    // 用于将像素点的值限制在指定范围内
+    let clamp = |value: f32, min: f32, max: f32| -> f32 {
+        if value < min {
+            min
+        } else if value > max {
+            max
+        } else {
+            value
+        }
+    };
+    // 使用rayon的并行迭代器，并行处理每个图片
+    fs::read_dir(manga_dir)
+        .unwrap()
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .try_for_each(|entry| -> Result<(), image::ImageError> {
+            let entry = entry?;
+            let in_image_path = entry.path();
+            let out_image_path = output_dir.join(
+                in_image_path
+                    .file_name()
+                    .expect("Failed to get in image file name"),
+            );
+            let in_img = image::open(&in_image_path)
+                .expect("Failed to open output image")
+                .to_rgb8();
+            // 创建一个新的图片缓冲区，用于存储去除水印后的图片
+            let mut img_buf = ImageBuffer::new(in_img.width(), in_img.height());
+            // 这里如果用rayon的并行反而会导致性能下降，所以使用普通的for循环
+            for y in 0..in_img.height() {
+                for x in 0..in_img.width() {
+                    let [in_r, in_g, in_b] = in_img.get_pixel(x, y).0;
+                    let [black_r, black_g, black_b] = black.get_pixel(x, y).0;
+                    let [white_r, white_g, white_b] = white.get_pixel(x, y).0;
+
+                    let watermark_removed_pixel = [
+                        (in_r as f32 - black_r as f32) / ((white_r - black_r) as f32 / 255.0),
+                        (in_g as f32 - black_g as f32) / ((white_g - black_g) as f32 / 255.0),
+                        (in_b as f32 - black_b as f32) / ((white_b - black_b) as f32 / 255.0),
+                    ];
+
+                    let clamped_pixel = image::Rgb([
+                        clamp(watermark_removed_pixel[0], 0.0, 255.0) as u8,
+                        clamp(watermark_removed_pixel[1], 0.0, 255.0) as u8,
+                        clamp(watermark_removed_pixel[2], 0.0, 255.0) as u8,
+                    ]);
+
+                    img_buf.put_pixel(x, y, clamped_pixel);
+                }
+            }
+            // 保存去除水印后的图片
+            let watermark_removed_image = RgbImage::from(img_buf);
+            let encoder = jpeg_encoder::Encoder::new_file(out_image_path, 97).unwrap();
+            encoder
+                .encode(
+                    &watermark_removed_image.as_raw(),
+                    watermark_removed_image.width() as u16,
+                    watermark_removed_image.height() as u16,
+                    jpeg_encoder::ColorType::Rgb,
+                )
+                .unwrap();
+            Ok(())
+        })
+        .unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
             generate_background,
             read_file,
+            remove_watermark
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,78 @@
 <script setup lang="ts">
 import BackgroundMaker from "./components/BackgroundMaker.vue";
+import {invoke} from "@tauri-apps/api/core";
+import {ref} from "vue";
+import {open} from "@tauri-apps/plugin-dialog";
+
+const whitePath = ref<string>();
+const blackPath = ref<string>();
+const mangaDir = ref<string>();
+const outputDir = ref<string>();
+
+
+async function removeWatermark() {
+  if (whitePath.value === undefined) {
+    console.error("请选择白底水印图片")
+    return;
+  }
+  if (blackPath.value === undefined) {
+    console.error("请选择黑底水印图片")
+    return;
+  }
+  if (mangaDir.value === undefined) {
+    console.error("请选择漫画文件夹")
+    return;
+  }
+  if (outputDir.value === undefined) {
+    console.error("请选择输出文件夹")
+    return;
+  }
+
+  await invoke("remove_watermark", {
+    whitePath: whitePath.value,
+    blackPath: blackPath.value,
+    mangaDir: mangaDir.value,
+    outputDir: outputDir.value,
+  });
+}
+
+async function selectWhitePath() {
+  const fileResponse = await open();
+  if (fileResponse === null) {
+    return;
+  }
+
+  whitePath.value = fileResponse.path;
+}
+
+async function selectBlackPath() {
+  const fileResponse = await open();
+  if (fileResponse === null) {
+    return;
+  }
+
+  blackPath.value = fileResponse.path;
+}
+
+async function selectMangaDir() {
+  const dirPath = await open({directory: true});
+  if (dirPath === null) {
+    return;
+  }
+
+  mangaDir.value = dirPath;
+}
+
+async function selectOutputDir() {
+  const dirPath = await open({directory: true});
+  if (dirPath === null) {
+    return;
+  }
+
+  outputDir.value = dirPath;
+}
+
+
 </script>
 
 <template>
@@ -7,4 +80,9 @@ import BackgroundMaker from "./components/BackgroundMaker.vue";
     <background-maker :is-black="true"/>
     <background-maker :is-black="false"/>
   </div>
+  <button @click="selectWhitePath">选择白底水印图片</button>
+  <button @click="selectBlackPath">选择黑底水印图片</button>
+  <button @click="selectMangaDir">选择漫画文件夹</button>
+  <button @click="selectOutputDir">选择输出文件夹</button>
+  <button @click="removeWatermark">去水印</button>
 </template>
