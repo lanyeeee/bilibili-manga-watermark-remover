4947a89fcb70da1dbd87a34272a269d5ccf5323a
lanyeeee <1210347077@qq.com>
2024-06-30 09:06:04 +0800

    chore: 格式化代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite"
-import Components from "unplugin-vue-components/vite"
-import {NaiveUiResolver} from "unplugin-vue-components/resolvers"
-import UnoCSS from "unocss/vite"
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import {NaiveUiResolver} from "unplugin-vue-components/resolvers";
+import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
