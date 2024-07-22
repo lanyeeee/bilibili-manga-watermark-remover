7aa0d5e8e148cc0e45823187fff7146e7ad85dff
lanyeeee <1210347077@qq.com>
2024-07-22 11:36:36 +0800

    feat: 优化去水印进度显示

---
+++ src/components/RemoveProgress.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps<{
+  removeWatermarkTasks: Map<string, [number, number]>;
+}>();
+
+const tasksProgress = computed(() =>
+    Array.from(props.removeWatermarkTasks)
+        .map(([dirPath, [current, total]]) => ({
+          dirPath,
+          percentage: Math.round(current / total * 100),
+        }))
+);
+</script>
+
+<template>
+  <div>
+    <div v-for="(status, index) in tasksProgress" :key="index">
+      <n-progress :percentage="status.percentage">{{ status.percentage }}% {{ status.dirPath }}</n-progress>
+    </div>
+  </div>
+</template>
