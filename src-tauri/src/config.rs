8c4755e85f002caf731af41b4ead141e30b52182
lanyeeee <1210347077@qq.com>
2024-07-30 15:02:57 +0800

    feat: 前端支持选择`输出格式`和`体积优化`

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,12 +1,18 @@
+use crate::types::ImageFormat;
 use serde::{Deserialize, Serialize};
 use specta::Type;
 use std::path::PathBuf;
 use tauri::{AppHandle, Manager};
 
+#[allow(clippy::struct_field_names)]
 #[derive(Debug, Clone, Serialize, Deserialize, Type)]
 pub struct Config {
     #[serde(rename = "outputDir")]
     pub output_dir: PathBuf,
+    #[serde(rename = "outputFormat")]
+    pub output_format: ImageFormat,
+    #[serde(rename = "outputOptimize")]
+    pub output_optimize: bool,
 }
 
 impl Config {
@@ -15,6 +21,8 @@ impl Config {
         let config_path = resource_dir.join("config.json");
         let default_config = Config {
             output_dir: resource_dir,
+            output_format: ImageFormat::Jpeg,
+            output_optimize: false,
         };
         // 如果配置文件存在且能够解析，则使用配置文件中的配置，否则使用默认配置
         let config = if config_path.exists() {
@@ -27,7 +35,7 @@ impl Config {
 
         Ok(config)
     }
-
+    // FIXME: config_state需要被修改
     pub fn save(&self, app: &AppHandle) -> anyhow::Result<()> {
         let resource_dir = app.path().resource_dir()?;
         let config_path = resource_dir.join("config.json");
