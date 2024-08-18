a56e7e99230ad163c327a21ca17701adfaf58471
lanyeeee <1210347077@qq.com>
2024-08-18 16:20:43 +0800

    fix: 修复内容溢出屏幕后没有滚动条的问题

--- src/AppContent.vue
+++ src/AppContent.vue
@@ -6,11 +6,11 @@ import DownloadPane from "./components/DownloadPane.vue";
 </script>
 
 <template>
-  <n-tabs default-value="download" type="line" animated class="h-screen p-3">
-    <n-tab-pane class="h-full p-0!" name="download" tab="去水印" display-directive="show:lazy">
+  <n-tabs default-value="download" type="line" animated class="h-screen">
+    <n-tab-pane class="h-full overflow-auto" name="download" tab="去水印" display-directive="show:lazy">
       <watermark-pane></watermark-pane>
     </n-tab-pane>
-    <n-tab-pane class="h-full p-0!" name="export" tab="下载" display-directive="show:lazy">
+    <n-tab-pane class="h-full overflow-auto" name="export" tab="下载" display-directive="show:lazy">
       <download-pane></download-pane>
     </n-tab-pane>
   </n-tabs>
