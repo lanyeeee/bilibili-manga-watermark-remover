96e90bffc1a41274b6da1b47460e7544779aee66
lanyeeee <1210347077@qq.com>
2024-08-20 06:55:34 +0800

    feat: 漫画搜索功能

---
+++ src/components/DownloadComponents/DownloadingList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    下载列表
+    <n-progress type="line" :percentage="60" indicator-placement="inside"/>
+    <n-progress type="line" :percentage="60" indicator-placement="inside"/>
+    <n-progress type="line" :percentage="60" indicator-placement="inside"/>
+    <n-progress type="line" :percentage="60" indicator-placement="inside"/>
+    <n-progress type="line" :percentage="60" indicator-placement="inside"/>
+    <n-progress type="line" :percentage="60" indicator-placement="inside"/>
+  </div>
+</template>
