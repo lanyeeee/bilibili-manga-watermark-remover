b69ea3bd65b4250d9a99f51154ff32b8d7d6ec03
lanyeeee <1210347077@qq.com>
2024-10-18 04:50:16 +0800

    fix: 修复windows下调试时的`1420`端口占用问题

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(async () => ({
     clearScreen: false,
     // 2. tauri expects a fixed port, fail if that port is not available
     server: {
-        port: 1420,
+        port: 5005,
         strictPort: true,
         watch: {
             // 3. tell vite to ignore watching `src-tauri`
