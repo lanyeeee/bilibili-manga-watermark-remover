ea1b7f0eef0bbc2ba996b7cb9341926a869f3bb2
lanyeeee <lanyeeeeee@gmail.com>
2025-03-23 11:28:09 +0800

    feat: 添加`png` `webp` `gif`格式的支持

--- src-tauri/src/commands/open_image.rs
+++ src-tauri/src/commands/open_image.rs
@@ -3,12 +3,12 @@ use std::path::PathBuf;
 use anyhow::Context;
 
 use crate::errors::CommandResult;
-use crate::types::{JpgImageData, JpgImageInfo};
+use crate::types::{ImageData, ImageInfo};
 
 #[tauri::command(async)]
 #[specta::specta]
 #[allow(clippy::cast_possible_truncation)]
-pub fn open_image(path: String) -> CommandResult<JpgImageData> {
+pub fn open_image(path: String) -> CommandResult<ImageData> {
     let path = PathBuf::from(path);
     let (width, height) = image::image_dimensions(&path)
         .context(format!("获取图片 {path:?} 的尺寸失败"))
@@ -16,9 +16,9 @@ pub fn open_image(path: String) -> CommandResult<JpgImageData> {
     let data = std::fs::read(&path)
         .context(format!("读取图片 {path:?} 失败"))
         .map_err(anyhow::Error::from)?;
-    // JpgImage对象
-    let data = JpgImageData {
-        info: JpgImageInfo {
+
+    let data = ImageData {
+        info: ImageInfo {
             width,
             height,
             path,
